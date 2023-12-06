99e871287599148be03fc67bfb01f1f28afe03cf
Batyrkhan <syrymbatyrhan@gmail.com>
2023-12-06 11:07:12 +0300

    Saved by Batyrkhan

---
+++ Assignment14/src/App.vue
@@ -0,0 +1,18 @@
+htmlCopy code
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <game-board></game-board>
+  </div>
+</template>
+
+<script>
+import GameBoard from './components/Board.vue';
+
+export default {
+  components: {
+    GameBoard
+  }
+};
+</script>
+
